bc77dcddbcdc2e323319fa0a9643cab31ba292e2
Cláudio Dadda <j.claudiopvh@hotmail.com>
2026-05-21 11:01:30 -0400

    Ajustes para funcionamento da API
    Ajuste no:
    from google.oauth2.credentials import Credentials

--- app_web_v3.1.py
+++ app_web_v3.1.py
@@ -8,7 +8,8 @@ import re
 
 import pandas as pd 
 import gspread
-from google.oauth2.service_account import Credentials
+#from google.oauth2.service_account import Credentials
+from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseUpload
 
